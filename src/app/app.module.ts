f051c4f6e1f431e10bb89570bf1f6ea4c4e34b3e
aydiallo <diallo.alpha-yankhouba@ugb.edu.sn>
2019-02-04 10:37:17 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZoneComponent } from './zone/zone.component';
+import { ClientComponent } from './client/client.component';
+import { CompteComponent } from './compte/compte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZoneComponent,
+    ClientComponent,
+    CompteComponent
   ],
   imports: [
     BrowserModule,
